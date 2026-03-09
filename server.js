d55d9fc44d985f6d62a81a81488211b1f097e0e0
tristantheguy <92645917+tristantheguy@users.noreply.github.com>
2026-03-09 04:33:07 -0400

    Fix broken site links and static serving setup

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const app = express();
 
 app.use(cors({ origin: 'http://www.marksnpcgenerator.ga' }));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static(__dirname + '/public'));
+// Serve all website files from the repo root. Some pages and assets are not in /public.
+app.use(express.static(__dirname));
 
 app.listen(3000, function() {
   console.log('Web server listening on port 3000');
